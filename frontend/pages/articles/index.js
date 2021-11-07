2f5e303da6ba2e507b0a3d9ada14762107f45068
dangvanduc1999 <vanduclipi@gmail.com>
2021-11-07 19:13:53 +0700

    task4

--- frontend/pages/articles/index.js
+++ frontend/pages/articles/index.js
@@ -6,12 +6,12 @@ import { config } from "../../config";
 import { getArticles, countArticle } from "../../services/article.service";
 import { usePagination } from "../../utils/hooks/usePagination";
 import { createQueryString, formatQueryParams } from "../../utils/common";
+import Banner from "../../components/Banner";
+import { fetchAPI } from "../../utils/api";
 
-function BlogPage({ articles, _limit, _start, totalCount }) {
+function BlogPage({ articles, _limit, _start, totalCount, blogsBanner }) {
   const router = useRouter();
   const query = router.query;
-
-  // console.log(articles)
 
   const { currentPage, itemPerPage, totalItems } = usePagination(
     _start,
@@ -21,6 +21,7 @@ function BlogPage({ articles, _limit, _start, totalCount }) {
 
   return (
     <div>
+      <Banner bannerData={blogsBanner} />
       <Blog
         articles={articles}
         currentPage={currentPage}
@@ -36,9 +37,10 @@ export default BlogPage;
 export async function getServerSideProps({ params = {}, query = {} }) {
   const formatQuery = formatQueryParams(query);
   let { _limit, _start, _category_id } = formatQuery;
-  const [articles, totalCount] = await Promise.all([
+  const [articles, totalCount, blogsBanner] = await Promise.all([
     getArticles(query),
-    countArticle(query)
+    countArticle(query),
+    fetchAPI("/article-banner")
   ]);
-  return { props: { articles, _limit, _start, totalCount } };
+  return { props: { articles, _limit, _start, totalCount, blogsBanner } };
 }
