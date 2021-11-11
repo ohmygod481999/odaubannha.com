bd2284f2f5dc17d58ab4709b6e463ead6606c51a
longvb <vuongbaolong48@gmail.com>
2021-11-11 17:43:46 +0700

    add title, edit projectBanner name

--- frontend/pages/articles/index.js
+++ frontend/pages/articles/index.js
@@ -8,39 +8,43 @@ import { usePagination } from "../../utils/hooks/usePagination";
 import { createQueryString, formatQueryParams } from "../../utils/common";
 import Banner from "../../components/Banner";
 import { fetchAPI } from "../../utils/api";
+import Head from "next/head";
 
 function BlogPage({ articles, _limit, _start, totalCount, blogsBanner }) {
-  const router = useRouter();
-  const query = router.query;
+    const router = useRouter();
+    const query = router.query;
 
-  const { currentPage, itemPerPage, totalItems } = usePagination(
-    _start,
-    _limit,
-    totalCount
-  );
+    const { currentPage, itemPerPage, totalItems } = usePagination(
+        _start,
+        _limit,
+        totalCount
+    );
 
-  return (
-    <div>
-      <Banner bannerData={blogsBanner} />
-      <Blog
-        articles={articles}
-        currentPage={currentPage}
-        itemPerPage={itemPerPage}
-        totalItems={totalItems}
-      />
-    </div>
-  );
+    return (
+        <div>
+            <Head>
+                <title>{blogsBanner.title}</title>
+            </Head>
+            <Banner bannerData={blogsBanner} />
+            <Blog
+                articles={articles}
+                currentPage={currentPage}
+                itemPerPage={itemPerPage}
+                totalItems={totalItems}
+            />
+        </div>
+    );
 }
 
 export default BlogPage;
 
 export async function getServerSideProps({ params = {}, query = {} }) {
-  const formatQuery = formatQueryParams(query);
-  let { _limit, _start, _category_id } = formatQuery;
-  const [articles, totalCount, blogsBanner] = await Promise.all([
-    getArticles(query),
-    countArticle(query),
-    fetchAPI("/article-banner")
-  ]);
-  return { props: { articles, _limit, _start, totalCount, blogsBanner } };
+    const formatQuery = formatQueryParams(query);
+    let { _limit, _start, _category_id } = formatQuery;
+    const [articles, totalCount, blogsBanner] = await Promise.all([
+        getArticles(query),
+        countArticle(query),
+        fetchAPI("/article-banner"),
+    ]);
+    return { props: { articles, _limit, _start, totalCount, blogsBanner } };
 }
--- frontend/pages/projects/index.js
+++ frontend/pages/projects/index.js
@@ -2,34 +2,38 @@ import React from "react";
 import Banner from "../../components/Banner";
 import ProductsList from "../../components/ProductsList";
 import { fetchAPI } from "../../utils/api";
+import Head from "next/head";
 
 function Projects({ products, categories, regions, projectsBanner }) {
-  return (
-    <div>
-      <Banner bannerData={projectsBanner} />
-      <ProductsList
-        products={products}
-        categories={categories}
-        regions={regions}
-      />
-    </div>
-  );
+    return (
+        <div>
+            <Head>
+                <title>{projectsBanner.title}</title>
+            </Head>
+            <Banner bannerData={projectsBanner} />
+            <ProductsList
+                products={products}
+                categories={categories}
+                regions={regions}
+            />
+        </div>
+    );
 }
 export const getServerSideProps = async () => {
-  const [products, categories, regions, projectsBanner] = await Promise.all([
-    fetchAPI("/products"),
-    fetchAPI("/categories"),
-    fetchAPI("/regions"),
-    fetchAPI("/project-banner")
-  ]);
+    const [products, categories, regions, projectsBanner] = await Promise.all([
+        fetchAPI("/products"),
+        fetchAPI("/categories"),
+        fetchAPI("/regions"),
+        fetchAPI("/project-banner"),
+    ]);
 
-  return {
-    props: {
-      products,
-      categories,
-      regions,
-      projectsBanner
-    }
-  };
+    return {
+        props: {
+            products,
+            categories,
+            regions,
+            projectsBanner,
+        },
+    };
 };
 export default Projects;
